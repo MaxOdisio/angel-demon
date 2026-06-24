4fa0cad02bf6a1481ad85d379ba63eadf6f408b8
Max <massimo.odisio@gmail.com>
2026-06-24 14:22:07 +0200

    adds cors for frontend

--- backend/src/constants/env.ts
+++ backend/src/constants/env.ts
@@ -1,7 +1,11 @@
 import 'dotenv/config';
 
 export const PORT = process.env.PORT || 3000;
+
 export const OPENAI_API_KEY = process.env.OPENAI_API_KEY || "";
 if (!OPENAI_API_KEY || OPENAI_API_KEY == "") {
   throw new Error("OPENAI_API_KEY not defined in .env");
 }
+
+// used for CORS
+export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import { handlerDebate } from './api/debate.js';
-import { PORT } from './constants/env.js';
+import { FRONTEND_URL, PORT } from './constants/env.js';
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+  origin: FRONTEND_URL
+}));
 
 // TODO add input validation (Zod?), error handling (extend Error class) and logging middlewares (rate limit, jwt + argon2)
 
